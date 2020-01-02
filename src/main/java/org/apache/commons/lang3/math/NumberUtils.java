4eba144db3c5bbfaaa6262ebcecbb470375ef06d
Debug-2020 <debug2020@protonmail.com>
2020-01-02 15:08:02 +0800

    4107-54418-Lang1-result1575164951284

--- src/main/java/org/apache/commons/lang3/math/NumberUtils.java
+++ src/main/java/org/apache/commons/lang3/math/NumberUtils.java
@@ -469,7 +469,9 @@ public class NumberUtils {
                 return createBigInteger(str);
             }
             // >¸ÄÎªÁË>=
-            if (hexDigits >= 8 && str.charAt(pfxLen) == '8') { // too many for an int
+            if (hexDigits >= 8 &&  (str.charAt(pfxLen) == '8' ||
+            		str.charAt(pfxLen) == '9' ||
+            		str.charAt(pfxLen) == 'F' )) { // too many for an int
                 return createLong(str);
             }
             return createInteger(str);
